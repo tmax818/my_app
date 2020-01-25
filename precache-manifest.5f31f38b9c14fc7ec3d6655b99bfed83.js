01a9d3c4b26b9ecf093274f00c8b812053114e7b
tmax818 <tylermaxwell661@gmail.com>
2020-01-24 18:21:03 -0800

    Updates

---
+++ precache-manifest.5f31f38b9c14fc7ec3d6655b99bfed83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2eda3c881ef0f66529d8221f90dd97",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b3241d4c0529523a80a3",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eecb3d9d0516d5a0a9d4",
+    "url": "/my-app/static/js/2.3dcb40ab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.3dcb40ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3241d4c0529523a80a3",
+    "url": "/my-app/static/js/main.462a62ac.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
